62b04ac9c5fe7fbec1f290b15463586cfab299e0
Dan Burnside <dburnside22@gmail.com>
2022-12-01 07:14:00 -0600

    So close tried sorting

--- day1/day1.js
+++ day1/day1.js
@@ -26,43 +26,78 @@ const part1 = () => {
 
 const part2 = () => {
     console.log('Welcome To Day 1 : Part 2');
-    let workingInput = testInput.split(/\r?\n\n/).filter(element => element);
+    let workingInput = input.split(/\r?\n\n/).filter(element => element);
 
-    let highestNumber = 0;
-    let secondHighest = 0;
-    let thirdHighest = 0;
+ 
 
-    workingInput.forEach((numbers) => {
-        let changedANumber = false;
-
-        const numbersSplitOut = numbers.replaceAll('\n', ' ').trim().split(' ');
-        const totalOfNumbers = numbersSplitOut.reduce((number1, number2) => {
+    const sorted = workingInput.sort((a, b) => {
+        const ANumbersSplitOut = a.replaceAll('\n', ' ').trim().split(' ');
+        const ATotalOfNumbers = ANumbersSplitOut.reduce((number1, number2) => {
+            return Number(number1) + Number(number2);
+        });
+        const BNumbersSplitOut = b.replaceAll('\n', ' ').trim().split(' ');
+        const BTotalOfNumbers = BNumbersSplitOut.reduce((number1, number2) => {
             return Number(number1) + Number(number2);
         });
 
-        if (totalOfNumbers > highestNumber) {
-            highestNumber = totalOfNumbers;
-            changedANumber = true;
+        if (ATotalOfNumbers > BTotalOfNumbers) {
+            return -1;
+        } else if (ATotalOfNumbers < BTotalOfNumbers) {
+            return 1;
+        } else {
+            return 0;
         }
-        if (totalOfNumbers > secondHighest && !changedANumber) {
-            secondHighest = totalOfNumbers;
-        }
-        if (totalOfNumbers > thirdHighest && !changedANumber) {
-            thirdHighest = totalOfNumbers;
-        }
+    });
 
-    })
-    console.log(`Highest number is now ${highestNumber}`);
-    console.log(`Second highest number is now ${secondHighest}`);
-    console.log(`Third highest number is now ${thirdHighest}`);
-    console.log(`The total is ${highestNumber + secondHighest + thirdHighest}`)
+    console.log(sorted);
+    let highestNumber = sorted[0].replaceAll('\n', ' ').trim().split(' ');
+    let highestTotal = highestNumber.reduce((number1, number2) => {
+        return Number(number1) + Number(number2);
+    });
+    let secondNumber = sorted[1].replaceAll('\n', ' ').trim().split(' ');
+    let secondTotal = secondNumber.reduce((number1, number2) => {
+        return Number(number1) + Number(number2);
+    });
+    let thirdNumber = sorted[2].replaceAll('\n', ' ').trim().split(' ');
+    let thirdTotal = thirdNumber.reduce((number1, number2) => {
+        return Number(number1) + Number(number2);
+    });
+
+    console.log(`Highest number is now ${highestTotal}`);
+    console.log(`Second highest number is now ${secondTotal}`);
+    console.log(`Third highest number is now ${thirdTotal}`);
+    console.log(`${highestTotal + secondTotal + thirdTotal}`)
+
+    // workingInput.forEach((numbers) => {
+    //     let changedANumber = false;
+
+    //     const numbersSplitOut = numbers.replaceAll('\n', ' ').trim().split(' ');
+    //     const totalOfNumbers = numbersSplitOut.reduce((number1, number2) => {
+    //         return Number(number1) + Number(number2);
+    //     });
+
+    //     if (totalOfNumbers > highestNumber) {
+    //         highestNumber = totalOfNumbers;
+    //         changedANumber = true;
+    //     } else if (totalOfNumbers > secondHighest) {
+    //         secondHighest = totalOfNumbers;
+    //     } else if (totalOfNumbers > thirdHighest) {
+    //         thirdHighest = totalOfNumbers;
+    //     }
+
+    // })
+    // console.log(`Highest number is now ${highestNumber}`);
+    // console.log(`Second highest number is now ${secondHighest}`);
+    // console.log(`Third highest number is now ${thirdHighest}`);
+    // console.log(`The total is ${highestNumber + secondHighest + thirdHighest}`)
 
 }
 
 part2();
 
 
-// Wrong Guess 207645
+// Wrong Guesses 207645
+// 713006924969142
 
 
 
