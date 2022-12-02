01951d1164140cf13b844f867a2544ba1a56677c
Dan Burnside <dburnside22@gmail.com>
2022-12-02 06:06:37 -0600

    stub out problem and add input

--- day2/day2.js
+++ day2/day2.js
@@ -1,7 +1,15 @@
 import {input, testInput} from './input.js';
 
 const part1 = () => {
-    console.log('Welcome To Day 2')
+    console.log('Welcome To Day 2');
+
+    // A || X = Rock and + 1
+    // B || Y = Scisors and + 3
+    // C || Z = Paper and + 2
+
+    // Win = 6
+    // Draw = 3
+    // Loss = 0
 }
 
 part1();
