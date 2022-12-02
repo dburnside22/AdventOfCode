1a4eba17357159c055c8e8c46ecf90c409fe154d
Dan Burnside <daniel.burnside@daveramsey.com>
2022-12-02 09:08:32 -0600

    Complete Day2

--- day2/day2.js
+++ day2/day2.js
@@ -51,11 +51,68 @@ const part1 = () => {
 };
 
 // part1();
-}
 
-part1();
+const part2 = () => {
+  console.log("Welcome To Day 2: Part 2");
+  const calulateWin = (n1, n2) => {
+    let returnedTotal = 0;
 
+    if (n2 === "X") {
+      if (n1 === "A") {
+        returnedTotal += 3;
+      }
+      if (n1 === "B") {
+        returnedTotal += 1;
+      }
+      if (n1 === "C") {
+        returnedTotal += 2;
+      }
+    }
+    if (n2 === "Y") {
+      if (n1 === "A") {
+        returnedTotal += 1;
+      }
+      if (n1 === "B") {
+        returnedTotal += 2;
+      }
+      if (n1 === "C") {
+        returnedTotal += 3;
+      }
+      returnedTotal += 3;
+    }
+    if (n2 === "Z") {
+      if (n1 === "A") {
+        returnedTotal += 2;
+      }
+      if (n1 === "B") {
+        returnedTotal += 3;
+      }
+      if (n1 === "C") {
+        returnedTotal += 1;
+      }
+      returnedTotal += 6;
+    }
+    // A || X = Rock and + 1
+    // C || Z = Scisors and + 3
+    // B || Y = Paper and + 2
 
+    // Win = 6
+    // Draw = 3
+    // Loss = 0
+    return returnedTotal;
+  };
 
+  const splitInput = input.split("\n");
 
+  let total = 0;
+  splitInput.forEach((set) => {
+    const splitSet = set.split(" ");
+    total += calulateWin(splitSet[0], splitSet[1]);
+  });
 
+  console.log(total);
+};
+
+part2();
+
+// 12513
