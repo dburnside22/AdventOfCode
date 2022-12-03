330e28bf64775abf543c7909668d3360c7803cbb
Dan Burnside <dburnside22@gmail.com>
2022-12-03 07:21:50 -0600

    Complete day3

--- day3/day3.js
+++ day3/day3.js
@@ -36,7 +36,47 @@ const part1 = () => {
     console.log('Total', total);
 }
 
-part1();
+// part1();
+
+const part2 = () => {
+    console.log('Welcome To Day 3');
+    let total = 0;
+
+    const scoreTheMatch = (letterToScore) => {
+        const score = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        const splitTheScore = score.split('');
+        splitTheScore.forEach((letter,i) => {
+            if (letter === letterToScore) {
+                total += i+1;
+            }
+        })
+    }
+
+    const findTheMatch = (elfOne, elfTwo, elfThree) => {
+        let matchingLetter = '';
+        const splitOne = elfOne.split('');
+        splitOne.forEach((letter) => {
+            if (elfTwo.includes(letter) && elfThree.includes(letter)) {
+                matchingLetter = letter;
+            }
+        })
+        scoreTheMatch(matchingLetter);
+    }
+
+    const arrayOfSacks = input.split('\n');
+
+    for (let i = 0; i < arrayOfSacks.length - 2; i += 3){
+        const firstElf = arrayOfSacks[i];
+        const secondElf = arrayOfSacks[i + 1];
+        const thirdElf = arrayOfSacks[i + 2];
+        
+        findTheMatch(firstElf, secondElf, thirdElf);
+    }
+    console.log('Total', total);
+}
+
+part2();
 
 
 
