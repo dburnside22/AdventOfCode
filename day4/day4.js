18081f4946b7d27c06bdd2ab0aa7eb9fa237d5ce
Dan Burnside <dburnside22@gmail.com>
2022-12-04 06:37:44 -0600

    complete part 1

--- day4/day4.js
+++ day4/day4.js
@@ -1,11 +1,25 @@
 import {input, testInput} from './input.js';
 
 const part1 = () => {
-    console.log('Welcome To Day 4')
+    console.log('Welcome To Day 4');
+    let totalOfOverlaps = 0;
+    const checkForOverlap = (r1, r2) => {
+       if((r1[0]>=r2[0] && r1[1]<=r2[1]) || (r2[0]>=r1[0] && r2[1]<=r1[1])){
+         totalOfOverlaps++
+        }
+    } 
+
+    const splitInput = input.split('\n').map(el => el.split(','))
+    splitInput.forEach((set) => {
+        console.log('set', set)
+        const firstElfRange = set[0].split('-').map(Number)
+        const secondElfRange = set[1].split('-').map(Number)
+        checkForOverlap(firstElfRange, secondElfRange)
+    })
+    console.log('totalOfOverlaps', totalOfOverlaps)
 }
 
 part1();
-
 
 
 
