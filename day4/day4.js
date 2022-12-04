56e5ee1dd32ccd222b0b2eaf0b1dc721f6690dd7
Dan Burnside <dburnside22@gmail.com>
2022-12-04 12:08:31 -0600

    Complete day4

--- day4/day4.js
+++ day4/day4.js
@@ -19,7 +19,28 @@ const part1 = () => {
     console.log('totalOfOverlaps', totalOfOverlaps)
 }
 
-part1();
+// part1();
+
+const part2 = () => {
+    console.log('Welcome To Day 4: part 2');
+    let totalOfOverlaps = 0;
+    const checkForNoOverlap = (r1, r2) => {
+       if((r1[0] >= r2[0] && r1[0] <= r2[1]) || (r1[1] >= r2[0] && r1[1] <= r2[1]) ||
+        (r2[0] >= r1[0] && r2[0] <= r1[1]) || (r2[1] >= r1[0] && r2[1] <= r1[1])){
+         totalOfOverlaps++
+        }
+    } 
+
+    const splitInput = input.split('\n').map(el => el.split(','))
+    splitInput.forEach((set) => {
+        const firstElfRange = set[0].split('-').map(Number)
+        const secondElfRange = set[1].split('-').map(Number)
+        checkForNoOverlap(firstElfRange, secondElfRange)
+    })
+    console.log('totalOfOverlaps', totalOfOverlaps)
+}
+
+part2();
 
 
 
