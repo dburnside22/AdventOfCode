b98ef2acf94c52bb99f77ccb2956d1aac118e01e
Dan Burnside <daniel.burnside@daveramsey.com>
2022-12-05 09:58:41 -0600

    complete part 1

--- day5/day5.js
+++ day5/day5.js
@@ -9,71 +9,120 @@ const part1 = () => {
     return brokenCrate[1];
   };
 
-  const addToRightArrays = (firstArrayIndex, stringToParse) => {
-    const splitString = stringToParse.split(" ");
-    let arrayItBelongs = firstArrayIndex;
-    // console.log("splitString", splitString);
-    splitString.forEach((crate, j) => {
-      const indexOfArrayToPushTo = arrayItBelongs + j;
-      if (crate != "") {
-        const crateToAdd = breakTheCrate(crate);
-        arrays[indexOfArrayToPushTo].push(crateToAdd);
-      } else {
-        arrayItBelongs--;
-      }
-    });
+  const addToRightArrays = (indexOfArray, crate) => {
+    arrays[indexOfArray].push(breakTheCrate(crate));
   };
 
-  testInput
+  input
     .split("\n")
     .reverse()
     .map((line, i) => {
-      const splitLine = line.split("  ");
-      console.log("splitLine", splitLine);
+      let index = 0;
       if (i == 0) {
-        splitLine.forEach(() => arrays.push([]));
+        line.split(" ").forEach(() => arrays.push([]));
       } else {
-        splitLine.forEach((group, i) => {
-          if (group != "") {
-            console.log("group", group);
-            console.log("i", i);
+        line.split(" ").map((crate) => {
+          if (crate != "") {
+            index++;
+            addToRightArrays(index - 1, crate);
+          } else {
+            index += 0.25;
           }
-          // addToRightArrays(i, group);
         });
       }
-      // console.log("line", line);
-      // console.log("arrays", arrays);
     });
 
-  // instructions.split("\n").map((instruction) => {
-  //   let ins = instruction
-  //     .replace("move", "")
-  //     .replace("from", "")
-  //     .replace("to", "")
-  //     .split("  ")
-  //     .map((t) => Number(t));
+  instructions.split("\n").map((instruction) => {
+    let ins = instruction
+      .replace("move", "")
+      .replace("from", "")
+      .replace("to", "")
+      .split("  ")
+      .map((t) => Number(t));
 
-  //   for (let i = 0; i < ins[0]; i++) {
-  //     const from = ins[1] - 1;
-  //     const to = ins[2] - 1;
+    for (let i = 0; i < ins[0]; i++) {
+      const from = ins[1] - 1;
+      const to = ins[2] - 1;
 
-  //     const removedCrate = arrays[from].pop();
-  //     arrays[to].push(removedCrate);
-  //   }
-  // });
+      const removedCrate = arrays[from].pop();
+      arrays[to].push(removedCrate);
+    }
+  });
 
-  // let finalString = "";
+  let finalString = "";
 
-  // arrays.forEach((array) => {
-  //   const addLetter = array[array.length - 1];
-  //   if (addLetter != undefined) {
-  //     finalString += array[array.length - 1];
-  //   }
-  // });
+  arrays.forEach((array) => {
+    const addLetter = array[array.length - 1];
+    if (addLetter != undefined) {
+      finalString += array[array.length - 1];
+    }
+  });
 
-  console.log("arrays", arrays);
+  console.log("finalString", finalString);
 };
 
-part1();
+// part1();
 
 //GJSBZDNW
+const part2 = () => {
+  console.log("Welcome To Day 5");
+  let arrays = [];
+
+  const breakTheCrate = (crateToBreak) => {
+    const brokenCrate = crateToBreak.split("");
+    return brokenCrate[1];
+  };
+
+  const addToRightArrays = (indexOfArray, crate) => {
+    arrays[indexOfArray].push(breakTheCrate(crate));
+  };
+
+  input
+    .split("\n")
+    .reverse()
+    .map((line, i) => {
+      let index = 0;
+      if (i == 0) {
+        line.split(" ").forEach(() => arrays.push([]));
+      } else {
+        line.split(" ").map((crate) => {
+          if (crate != "") {
+            index++;
+            addToRightArrays(index - 1, crate);
+          } else {
+            index += 0.25;
+          }
+        });
+      }
+    });
+
+  instructions.split("\n").map((instruction) => {
+    let ins = instruction
+      .replace("move", "")
+      .replace("from", "")
+      .replace("to", "")
+      .split("  ")
+      .map((t) => Number(t));
+
+    for (let i = 0; i < ins[0]; i++) {
+      const from = ins[1] - 1;
+      const to = ins[2] - 1;
+
+      const removedCrate = arrays[from].pop();
+      arrays[to].push(removedCrate);
+    }
+  });
+
+  let finalString = "";
+
+  arrays.forEach((array) => {
+    const addLetter = array[array.length - 1];
+    if (addLetter != undefined) {
+      finalString += array[array.length - 1];
+    }
+  });
+
+  console.log("finalString", finalString);
+};
+
+part2();
