f91adaf9cf80115ac40c4bb28a69f0dc58fcb17b
Dan Burnside <dburnside22@gmail.com>
2022-12-06 05:27:48 -0600

    complete part 1

--- day6/day6.js
+++ day6/day6.js
@@ -1,12 +1,32 @@
-import {input, testInput} from './input.js';
+import { input, testInput } from "./input.js";
 
 const part1 = () => {
-    console.log('Welcome To Day 6')
-}
+  console.log("Welcome To Day 6");
+  // let howLongItTook = 0;
+  const splitInput = input.split("");
+
+  let runningArray = [];
+  let answer;
+
+  for (let i = 0; i < splitInput.length - 3; i++) {
+    runningArray = [];
+    let first = splitInput[i];
+    let second = splitInput[i + 1];
+    let third = splitInput[i + 2];
+    let fourth = splitInput[i + 3];
+    if (first != second) {
+      runningArray.push(first);
+      runningArray.push(second);
+      if (!runningArray.includes(third)) {
+        runningArray.push(third);
+        if (!runningArray.includes(fourth)) {
+          answer = i + 4;
+          break;
+        }
+      }
+    }
+  }
+  console.log(answer);
+};
 
 part1();
-
-
-
-
-
