c87a746bc59c5641ebc2140b90db4cdb1c3cacfe
Dan Burnside <daniel.burnside@daveramsey.com>
2022-12-07 15:04:20 -0600

    Complete day7

--- day7/day7.js
+++ day7/day7.js
@@ -71,10 +71,87 @@ const part1 = (dataSource) => {
 };
 
 // part1(testInput);
-part1(input);
+// part1(input);
 
 // 827083 to low
 // 8693604 to high
 // 1705869
 
 // 1501149 the right answer
+
+const part2 = (dataSource) => {
+  console.log("Welcome To Day 7");
+  const splitInput = dataSource.split("\n");
+
+  let directoryTotals = [];
+  let currentDirectory = [];
+  let smallestOption = 0;
+  let availableSpace = 70000000;
+
+  const navigateToDirectory = (whereToNavigate) => {
+    if (whereToNavigate[2] == "..") {
+      currentDirectory.pop();
+    } else {
+      currentDirectory.push(whereToNavigate[2]);
+    }
+  };
+
+  const processDirectories = (dirs) => {
+    availableSpace -= dirs["/"];
+    for (let key in dirs) {
+      if (availableSpace + dirs[key] >= 30000000) {
+        if (dirs[key] < smallestOption || smallestOption == 0) {
+          smallestOption = dirs[key];
+        }
+      }
+    }
+  };
+
+  const addToAllDirectories = (numToAdd) => {
+    let fileName = "";
+    for (let i = 0; i < currentDirectory.length; i++) {
+      fileName += currentDirectory[i];
+      if (!directoryTotals[fileName]) {
+        directoryTotals[fileName] = numToAdd;
+      } else {
+        directoryTotals[fileName] += numToAdd;
+      }
+    }
+  };
+
+  const processList = (files) => {
+    let total = 0;
+    files.forEach((file) => {
+      if (file[0] != "dir") {
+        total += Number(file[0]);
+      }
+    });
+    addToAllDirectories(total);
+  };
+
+  let arrayOfFiles = [];
+
+  splitInput.forEach((line) => {
+    const splitLine = line.split(" ");
+    if (splitLine[0] == "$") {
+      if (arrayOfFiles.length > 0) {
+        processList(arrayOfFiles);
+        arrayOfFiles = [];
+      }
+      if (splitLine[1] == "cd") {
+        navigateToDirectory(splitLine);
+      }
+      if (splitLine[1] == "ls") {
+        // do nothing
+      }
+    } else {
+      arrayOfFiles.push(splitLine);
+    }
+  });
+  processList(arrayOfFiles);
+  processDirectories(directoryTotals);
+  console.log("grandTotal", smallestOption);
+};
+
+// part2(testInput);
+part2(input);
