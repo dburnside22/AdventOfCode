e2f1c0d1fab451a683b07739c6c90f588e1ce0cd
Dan Burnside <dburnside22@gmail.com>
2022-12-09 05:51:38 -0600

    wip part 1

--- day9/day9.js
+++ day9/day9.js
@@ -1,14 +1,59 @@
-import {input, testInput} from './input.js';
+import { input, testInput } from "./input.js";
 
 const part1 = (dataSource) => {
-    console.log('Welcome To Day 9');
-    const splitInput = dataSource.split('');
-}
+  console.log("Welcome To Day 9");
+  const splitInput = dataSource.split("\n").map((line) =>
+    line.split(" ").map((l, i) => {
+      if (i == 1) {
+        return Number(l);
+      } else {
+        return l;
+      }
+    })
+  );
+  let placesTailHit = [];
+  let positionOfTail = [0, 0];
+  let positionOfHead = [0, 0];
+  placesTailHit.push(positionOfTail);
+
+  const checkOnTail = () => {
+    const headAndTailOverLap =
+      positionOfHead[0] == positionOfTail[0] &&
+      positionOfHead[1] == positionOfTail[1];
+
+    const headIsToFar =
+      positionOfHead[0] - positionOfTail[0] > 1 ||
+      positionOfHead[0] - positionOfTail[0] < -1 ||
+      positionOfHead[1] - positionOfTail[1] > 1 ||
+      positionOfHead[1] - positionOfTail[1] < -1 ||
+      positionOfHead[0] - positionOfTail[1] > 1 ||
+      positionOfHead[0] - positionOfTail[1] < -1 ||
+      positionOfHead[1] - positionOfTail[0] > 1 ||
+      positionOfHead[1] - positionOfTail[0] < -1;
+  };
+
+  const moveHead = (instruction) => {
+    for (let i = 0; i < instruction[1]; i++) {
+      if (instruction[0] == "R") {
+        positionOfHead[1]++;
+      }
+      if (instruction[0] == "L") {
+        positionOfHead[1]--;
+      }
+      if (instruction[0] == "U") {
+        positionOfHead[0]++;
+      }
+      if (instruction[0] == "D") {
+        positionOfHead[0]--;
+      }
+      checkOnTail();
+    }
+  };
+
+  splitInput.forEach((instruction) => {
+    moveHead(instruction);
+  });
+};
 
 part1(testInput);
 // part1(input);
-
-
-
-
-
