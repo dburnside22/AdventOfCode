6311c6da4ce48d7034bc8939fbd61ad65a612f7c
Dan Burnside <dburnside22@gmail.com>
2022-12-10 22:21:15 -0600

    Complete day10

--- day10/day10.js
+++ day10/day10.js
@@ -38,4 +38,54 @@ const part1 = (dataSource) => {
 
 // part1(testInput1);
 // part1(testInput);
-part1(input);
+// part1(input);
+
+const part2 = (dataSource) => {
+  console.log("Welcome To Day 10: part 2");
+  const splitInput = dataSource.split("\n").map((i) =>
+    i.split(" ").map((l, j) => {
+      if (j == 1) {
+        return Number(l);
+      } else {
+        return l;
+      }
+    })
+  );
+  const importCycles = [40, 80, 120, 160, 200, 240];
+  let CRTdrawingPosition = 0;
+  let X = 1;
+
+  let picture = "";
+
+  splitInput.forEach((ins) => {
+    const check = () => {
+      if (
+        CRTdrawingPosition == X ||
+        CRTdrawingPosition == X - 1 ||
+        CRTdrawingPosition == X + 1
+      ) {
+        picture += "|";
+      } else {
+        picture += " ";
+      }
+      CRTdrawingPosition++;
+      if (importCycles.includes(CRTdrawingPosition)) {
+        console.log(picture);
+        picture = "";
+        X += 40;
+      }
+    };
+    if (ins[0] == "noop") {
+      check();
+      // do nothing be nothing
+    }
+    if (ins[0] == "addx") {
+      check();
+      check();
+      X += ins[1];
+    }
+  });
+};
+
+// part2(testInput);
+part2(input);
