214c2cb11810faf1e63fca351ca3e542779cc5f8
Dan Burnside <daniel.burnside@daveramsey.com>
2022-12-27 10:13:25 -0600

    Complete day13

--- day13/day13.js
+++ day13/day13.js
@@ -61,7 +61,7 @@ const part1 = (dataSource) => {
 };
 
 // part1(testInput);
-part1(input);
+// part1(input);
 
 // 462 to low
 // 1570 to low
@@ -74,3 +74,72 @@ part1(input);
 // 5331
 // 5319
 // The right answer 6623!!!!!!!!!!!!
+
+const part2 = (dataSource) => {
+  console.log("Welcome To Day 13: part 2");
+  // split on new line and filter out the blank lines
+  const splitInput = dataSource
+    .split("\n")
+    .filter((l) => l)
+    .map((k) => JSON.parse(k));
+  splitInput.push([[2]]);
+  splitInput.push([[6]]);
+
+  const processValues = (left, right, result) => {
+    const leftIsNumber = typeof left === "number";
+    const rightIsNumber = typeof right === "number";
+    if (leftIsNumber && rightIsNumber) {
+      if (left < right) {
+        result.rightOrder = true;
+        return;
+      }
+      if (left > right) {
+        result.rightOrder = false;
+        return;
+      }
+    } else if (!leftIsNumber && !rightIsNumber) {
+      let i = 0;
+      while (true) {
+        if (i > left.length - 1 && i <= right.length - 1) {
+          // left runs out first
+          result.rightOrder = true;
+          return;
+        } else if (i <= left.length - 1 && i > right.length - 1) {
+          // right runs out first
+          result.rightOrder = false;
+          return;
+        } else if (i > left.length - 1 && i > right.length - 1) {
+          // both runs out
+          return;
+        }
+        processValues(left[i], right[i], result);
+
+        if (typeof result.rightOrder !== "undefined") {
+          return;
+        }
+        i++;
+      }
+    } else {
+      if (leftIsNumber) {
+        processValues([left], right, result);
+      } else {
+        processValues(left, [right], result);
+      }
+    }
+  };
+
+  splitInput.sort((first, second) => {
+    const result = {};
+    processValues(first, second, result);
+    return result.rightOrder ? -1 : 1;
+  });
+  const strings = splitInput.map((x) => JSON.stringify(x));
+
+  const position1 = strings.indexOf("[[2]]") + 1;
+  const position2 = strings.indexOf("[[6]]") + 1;
+
+  console.log(position1 * position2);
+};
+
+// part2(testInput);
+part2(input);
