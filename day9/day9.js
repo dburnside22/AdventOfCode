932fd5b39bb27b0f5382bd58550738cefc38adf5
Dan Burnside <daniel.burnside@daveramsey.com>
2022-12-29 15:30:41 -0600

    complete part 1

--- day9/day9.js
+++ day9/day9.js
@@ -2,153 +2,79 @@ import { input, testInput } from "./input.js";
 
 const part1 = (dataSource) => {
   console.log("Welcome To Day 9");
-  const splitInput = dataSource.split("\n").map((line) =>
-    line.split(" ").map((l, i) => {
-      if (i == 1) {
-        return Number(l);
-      } else {
-        return l;
-      }
-    })
-  );
-  let placesTailHit = [];
-  let positionOfTail = [0, 0];
-  let positionOfHead = [0, 0];
-  placesTailHit.push(`${positionOfTail[0]}${positionOfTail[1]}`);
-
-  const moveTail = () => {
-    const up = positionOfHead[1] - positionOfTail[1] > 0;
-    const down = positionOfHead[1] - positionOfTail[1] < 0;
-    const left = positionOfHead[0] - positionOfTail < 0;
-    const right = positionOfHead[0] - positionOfTail > 0;
-
-    const upToRight = up && right;
-
-    const downToRight = down && right;
-
-    const upToLeft = up && left;
-
-    const downToLeft = down && left;
-
-    const movedDiagonal = upToRight || downToLeft || upToLeft || downToRight;
-
-    if (!movedDiagonal) {
-      if (positionOfHead[0] - positionOfTail[0] > 1) {
-        positionOfTail[0]++;
-      }
-      if (positionOfHead[0] - positionOfTail[0] < -1) {
-        positionOfTail[0]--;
-      }
-      if (positionOfHead[1] - positionOfTail[1] > 1) {
-        positionOfTail[1]++;
-      }
-      if (positionOfHead[1] - positionOfTail[1] < -1) {
-        positionOfTail[1]--;
-      }
-    }
-    if (upToRight) {
-      positionOfTail[0]++;
-      positionOfTail[1]++;
-    }
-    if (downToRight) {
-      positionOfTail[0]++;
-      positionOfTail[1]--;
-    }
-    if (upToLeft) {
-      positionOfTail[0]--;
-      positionOfTail[1]++;
-    }
-    if (downToLeft) {
-      positionOfTail[0]--;
-      positionOfTail[1]--;
-    }
-    console.log("positionsOfHead", positionOfHead);
-    console.log("positionOfTail", positionOfTail);
-
-    if (!placesTailHit.includes(`${positionOfTail[0]}${positionOfTail[1]}`)) {
-      placesTailHit.push(`${positionOfTail[0]}${positionOfTail[1]}`);
-    }
-    // console.log("placesTailHit", placesTailHit);
-  };
-
-  const checkOnTail = () => {
-    const headAndTailOverLap =
-      positionOfHead[0] == positionOfTail[0] &&
-      positionOfHead[1] == positionOfTail[1];
-
-    const movedUpDownLeftOrRight =
-      positionOfHead[0] - positionOfTail[0] > 0 ||
-      positionOfHead[0] - positionOfTail[0] < 0 ||
-      positionOfHead[1] - positionOfTail[1] > 0 ||
-      positionOfHead[1] - positionOfTail[1] < 0;
-
-    const up = positionOfHead[1] - positionOfTail[1] > 1;
-    const down = positionOfHead[1] - positionOfTail[1] < -1;
-
-    const upToRight =
-      (up &&
-        positionOfHead[0] - positionOfTail[0] > 1 &&
-        positionOfHead[1] - positionOfTail[1] == 1) ||
-      (positionOfHead[1] - positionOfTail[1] > 1 &&
-        positionOfHead[0] - positionOfTail[0] == 1);
-
-    const downToRight =
-      (down &&
-        positionOfHead[1] - positionOfTail[1] < -1 &&
-        positionOfHead[0] - positionOfTail[0] == 1) ||
-      (positionOfHead[0] - positionOfTail[0] < -1 &&
-        positionOfHead[1] - positionOfTail[1] == 1);
-
-    const upToLeft =
-      (up &&
-        positionOfHead[0] - positionOfTail[0] > -1 &&
-        positionOfHead[1] - positionOfTail[1] == 1) ||
-      (positionOfHead[1] - positionOfTail[1] > -1 &&
-        positionOfHead[0] - positionOfTail[0] == 1);
-
-    const downToLeft =
-      (down &&
-        positionOfHead[1] - positionOfTail[1] > -1 &&
-        positionOfHead[0] - positionOfTail[0] == -1) ||
-      (positionOfHead[0] - positionOfTail[0] > -1 &&
-        positionOfHead[1] - positionOfTail[1] == -1);
-
-    const movedDiagonal = upToRight || downToLeft || upToLeft || downToRight;
-
-    if (
-      (!headAndTailOverLap && (movedUpDownLeftOrRight || !movedDiagonal)) ||
-      movedDiagonal
-    ) {
-      moveTail();
-    }
-  };
-
-  const moveHead = (instruction) => {
-    for (let i = 0; i < instruction[1]; i++) {
-      if (instruction[0] == "R") {
-        positionOfHead[1]++;
-      }
-      if (instruction[0] == "L") {
-        positionOfHead[1]--;
-      }
-      if (instruction[0] == "U") {
-        positionOfHead[0]++;
-      }
-      if (instruction[0] == "D") {
-        positionOfHead[0]--;
-      }
-      checkOnTail();
-    }
-  };
-
-  splitInput.forEach((instruction) => {
-    moveHead(instruction);
+  const splitInput = dataSource.split("\n").map((line) => {
+    const [letter, number] = line.split(" ");
+    return {
+      direction: letter,
+      totalMoves: Number(number),
+    };
   });
-  console.log("totalOfPlacesTailHit", placesTailHit.length);
+  const visited = new Set();
+
+  const markVisited = (x, y, visited) => {
+    visited.add(`${x}-${y}`);
+  };
+  markVisited(0, 0, visited);
+
+  const directions = {
+    R: {
+      x: 1,
+      y: 0,
+    },
+    L: {
+      x: -1,
+      y: 0,
+    },
+    U: {
+      x: 0,
+      y: 1,
+    },
+    D: {
+      x: 0,
+      y: -1,
+    },
+  };
+  class Point {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+    move(direction) {
+      const delta = directions[direction];
+      this.x += delta.x;
+      this.y += delta.y;
+    }
+    follow(point) {
+      const distance = Math.max(
+        Math.abs(this.x - point.x),
+        Math.abs(this.y - point.y)
+      );
+      if (distance > 1) {
+        const directionX = point.x - this.x;
+        const directionY = point.y - this.y;
+        this.x += Math.abs(directionX) === 2 ? directionX / 2 : directionX;
+        this.y += Math.abs(directionY) === 2 ? directionY / 2 : directionY;
+      }
+    }
+  }
+
+  const head = new Point(0, 0);
+  const tail = new Point(0, 0);
+
+  for (const input of splitInput) {
+    for (let i = 0; i < input.totalMoves; i++) {
+      head.move(input.direction);
+      tail.follow(head);
+      markVisited(tail.x, tail.y, visited);
+    }
+  }
+
+  console.log(visited.size);
 };
 
-part1(testInput);
-// part1(input);
+// part1(testInput);
+part1(input);
 
 // 6487 to low
 // 6686 to high
+// 6494 right number!!
