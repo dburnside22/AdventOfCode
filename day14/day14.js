e7d5847a5c134d12749fa762557c28fbbd0b689e
Dan Burnside <daniel.burnside@daveramsey.com>
2022-12-29 15:30:47 -0600

    WIP part 1

--- day14/day14.js
+++ day14/day14.js
@@ -1,14 +1,12 @@
-import {input, testInput} from './input.js';
+import { input, testInput } from "./input.js";
 
 const part1 = (dataSource) => {
-    console.log('Welcome To Day 14');
-    const splitInput = dataSource.split('');
-}
+  console.log("Welcome To Day 14");
+  const splitInput = dataSource
+    .split("\n")
+    .map((l) => l.split("->").map((k) => k.trim()));
+  console.log(splitInput);
+};
 
 part1(testInput);
 // part1(input);
-
-
-
-
-
