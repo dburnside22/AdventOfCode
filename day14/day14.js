66f89c94c326632e9bef731f3a12cf133cd5eb75
Dan Burnside <daniel.burnside@daveramsey.com>
2022-12-29 08:45:41 -0600

    Generate day 14

---
+++ day14/day14.js
@@ -0,0 +1,14 @@
+import {input, testInput} from './input.js';
+
+const part1 = (dataSource) => {
+    console.log('Welcome To Day 14');
+    const splitInput = dataSource.split('');
+}
+
+part1(testInput);
+// part1(input);
+
+
+
+
+
