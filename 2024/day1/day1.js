48b3fe436a92befa413a6da888bd9254cd01c261
Daniel Burnside <daniel.burnside@daveramsey.com>
2024-12-04 11:12:49 -0600

    day 1 part 1

--- 2024/day1/day1.js
+++ 2024/day1/day1.js
@@ -2,11 +2,30 @@ import {input, testInput} from './input.js';
 
 const part1 = (dataSource) => {
     console.log('Welcome To Day 1');
-    const splitInput = dataSource.split('');
+    const splitInput = dataSource.split(/\n/);
+    const firstArray = [];
+    const secondArray = [];
+    splitInput.forEach(line => {
+        const numbers = line.match(/\d+/g);
+        const [first, second] = numbers.map(Number);
+        firstArray.push(first);
+        secondArray.push(second);
+    });
+    
+    let difference = 0;
+
+    const sortedFirstArray = firstArray.sort((a, b) => a - b);
+    const sortedSecondArray = secondArray.sort((a, b) => a - b);
+
+    sortedFirstArray.forEach((num, index) => {
+        difference += Math.abs(sortedSecondArray[index] - num);
+    });
+
+    console.log(difference);
 }
 
-part1(testInput);
-// part1(input);
+// part1(testInput);
+part1(input);
 
 // const part2 = (dataSource) => {
 //    console.log('Welcome To Day 1: Part 2');
