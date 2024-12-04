28e24bd92935cf102f16321bdd3ebfb19da3422b
Daniel Burnside <daniel.burnside@daveramsey.com>
2024-12-04 11:20:57 -0600

    finish day 1

--- 2024/day1/day1.js
+++ 2024/day1/day1.js
@@ -1,7 +1,34 @@
 import {input, testInput} from './input.js';
 
-const part1 = (dataSource) => {
-    console.log('Welcome To Day 1');
+// const part1 = (dataSource) => {
+//     console.log('Welcome To Day 1');
+//     const splitInput = dataSource.split(/\n/);
+//     const firstArray = [];
+//     const secondArray = [];
+//     splitInput.forEach(line => {
+//         const numbers = line.match(/\d+/g);
+//         const [first, second] = numbers.map(Number);
+//         firstArray.push(first);
+//         secondArray.push(second);
+//     });
+    
+//     let difference = 0;
+
+//     const sortedFirstArray = firstArray.sort((a, b) => a - b);
+//     const sortedSecondArray = secondArray.sort((a, b) => a - b);
+
+//     sortedFirstArray.forEach((num, index) => {
+//         difference += Math.abs(sortedSecondArray[index] - num);
+//     });
+
+//     console.log(difference);
+// }
+
+// // part1(testInput);
+// part1(input);
+
+const part2 = (dataSource) => {
+   console.log('Welcome To Day 1: Part 2');
     const splitInput = dataSource.split(/\n/);
     const firstArray = [];
     const secondArray = [];
@@ -12,28 +39,20 @@ const part1 = (dataSource) => {
         secondArray.push(second);
     });
     
-    let difference = 0;
+    const calculateScore = (array1, array2) => {
+        let score = 0;
+        array1.forEach((num) => {
+          const count = array2.filter((num2) => num2 === num).length;
+          score += num * count;
+        });
+        return score;
+    }
 
-    const sortedFirstArray = firstArray.sort((a, b) => a - b);
-    const sortedSecondArray = secondArray.sort((a, b) => a - b);
-
-    sortedFirstArray.forEach((num, index) => {
-        difference += Math.abs(sortedSecondArray[index] - num);
-    });
-
-    console.log(difference);
+    console.log(calculateScore(firstArray, secondArray));
 }
 
-// part1(testInput);
-part1(input);
-
-// const part2 = (dataSource) => {
-//    console.log('Welcome To Day 1: Part 2');
-//    const splitInput = dataSource.split('');
-// }
-
 // part2(testInput);
-// part2(input);
+part2(input);
 
 
 
