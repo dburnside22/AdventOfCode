adea79af8f096c8781199d6984ac1d06621e64c8
Daniel Burnside <daniel.burnside@daveramsey.com>
2024-12-13 11:20:50 -0600

    start day 3

---
+++ 2024/day3/day3.js
@@ -0,0 +1,22 @@
+import {input, testInput} from './input.js';
+
+const part1 = (dataSource) => {
+    console.log('Welcome To Day 3');
+    const splitInput = dataSource.split('');
+}
+
+part1(testInput);
+// part1(input);
+
+// const part2 = (dataSource) => {
+//    console.log('Welcome To Day 3: Part 2');
+//    const splitInput = dataSource.split('');
+// }
+
+// part2(testInput);
+// part2(input);
+
+
+
+
+
