c80101bcbf7527266549a4537bd16892ba52926e
Daniel Burnside <daniel.burnside@daveramsey.com>
2024-12-13 13:13:55 -0600

    complete part one

--- 2024/day3/day3.js
+++ 2024/day3/day3.js
@@ -2,11 +2,19 @@ import {input, testInput} from './input.js';
 
 const part1 = (dataSource) => {
     console.log('Welcome To Day 3');
-    const splitInput = dataSource.split('');
+    const regex = /mul\(\d+,\d+\)/g;
+    const matches = dataSource.match(regex);
+    let total = 0;
+    matches.forEach(match => {
+        const [a, b] = match.match(/\d+/g).map(Number);
+        total += a * b;
+    });
+
+    console.log('Total:', total);
 }
 
-part1(testInput);
-// part1(input);
+// part1(testInput);
+part1(input);
 
 // const part2 = (dataSource) => {
 //    console.log('Welcome To Day 3: Part 2');
