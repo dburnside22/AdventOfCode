dab2e4f6b478a73c28933cb24320848a11272469
Dan Burnside <dburnside22@gmail.com>
2025-12-03 10:54:41 -0600

    start day 3

---
+++ 2025/day3/day3.py
@@ -0,0 +1,24 @@
+from input import input_data, test_input
+
+def part1(data_source):
+    print('Welcome To Day 3')
+    # Split input into lines
+    lines = data_source.strip().split('\n')
+    
+    # Your solution here
+    pass
+
+if __name__ == '__main__':
+    part1(test_input)
+    # part1(input_data)
+    
+    # def part2(data_source):
+    #     print('Welcome To Day 3: Part 2')
+    #     lines = data_source.strip().split('\n')
+    #     
+    #     # Your solution here
+    #     pass
+    
+    # part2(test_input)
+    # part2(input_data)
+
---
+++ 2025/day3/input.py
@@ -0,0 +1,4 @@
+test_input = """"""
+
+input_data = """"""
+
